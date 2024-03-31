3618b55aeabd0f70396b0ddfe4d003a9c8a1b5c3
Lim Xiang Wei Alex <alex.lim.xiang.wei@gmail.com>
2024-04-01 06:08:59 +0800

    Added flask

--- Main.py
+++ Main.py
@@ -1,4 +1,5 @@
 import requests
+from flask import Flask, render_template, jsonify
 
 M5StickCPlus_IP_address = '192.168.116.77'  # change to your M5StickCPlus IP address
 
@@ -9,3 +10,48 @@ print(response.text)
 # Toggle emergency state
 response = requests.post(f'http://{M5StickCPlus_IP_address}/emergency')
 print(response.text)
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    # Retrieve sensor data from M5StickCPlus
+    response = requests.get(f'http://{M5StickCPlus_IP_address}/data')
+    data = response.text
+
+    # Parse the sensor data
+    acceleration = data.split('Acceleration: ')[1].split('\n')[0]
+    step_count = data.split('Step Count: ')[1].split('\n')[0]
+    fall_detected = data.split('Fall Detected: ')[1].split('\n')[0]
+    emergency = data.split('Emergency: ')[1].strip()
+
+    # Render the template with the sensor data
+    return render_template('index.html',
+                           acceleration=acceleration,
+                           step_count=step_count,
+                           fall_detected=fall_detected,
+                           emergency=emergency)
+
+@app.route('/data')
+def get_data():
+    # Retrieve sensor data from M5StickCPlus
+    response = requests.get(f'http://{M5StickCPlus_IP_address}/data')
+    data = response.text
+
+    # Parse the sensor data
+    acceleration = data.split('Acceleration: ')[1].split('\n')[0]
+    step_count = data.split('Step Count: ')[1].split('\n')[0]
+    fall_detected = data.split('Fall Detected: ')[1].split('\n')[0]
+    emergency = data.split('Emergency: ')[1].strip()
+
+    # Return the sensor data as JSON
+    return jsonify({
+        'acceleration': acceleration,
+        'step_count': step_count,
+        'fall_detected': fall_detected,
+        'emergency': emergency
+    })
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
