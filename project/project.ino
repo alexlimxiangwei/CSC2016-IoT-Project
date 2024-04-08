e0bcc68071dbc8a9e75d89503960e489a52553ac
Rymndt <2200805@sit.singaporetech.edu.sg>
2024-04-08 23:53:11 +0800

    Nil

--- project/project.ino
+++ project/project.ino
@@ -30,7 +30,7 @@ bool stepDetected = false;
 const char* mqttServer = "192.168.116.30";
 const int mqttPort = 1883;
 const char* registrationTopic = "m5stick/registration";
-String deviceName = "Alex";
+String deviceName = "GrandmaTim";
 String ipAddress;
 
 WiFiClient espClient;
@@ -222,7 +222,7 @@ void sendResponse(WiFiClient& client, const String& response, const String& cont
 void loop() {
 
   if (!pubSubClient.connected()) {
-        reconnect();
+        // reconnect();
     } else {
         pubSubClient.loop(); // Make sure to call this regularly to process incoming messages and maintain the connection
         sendKeepAlive();
